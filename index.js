6bd9d0894cebafa98f2854e122229169a56e9625
David Bohórquez <david.bohorquez@192.168.1.8>
2024-01-29 16:05:12 -0500

    Further improvements to the Planner view and general layout

--- index.js
+++ index.js
@@ -23,7 +23,17 @@ const addKoombeaStyles = () => {
   const css = `:root {
     --koombea-dark-green: #00343D;
     --koombea-green: #26666B;
+    --koombea-light-green: #0BD8A2;
+    --koombea-booking-confirmed: #8ACBF9;
+    --koombea-booking-pto: #F2AB52;
+    --koombea-booking-internal: #C38CEA;
+    --koombea-booking-tentative: #B8B8B8;
+    --koombea-booking-overbook: #CC2E3B;
+    --koombea-booking-holiday: #F2F2F2;
+    --koombea-booking-available: #E6E6E6;
     --white: #FFFFFF;
+    --link: #1D979F;
+    --link-hover: #14686E;
   }
 
   .item-creation,
@@ -35,15 +45,63 @@ const addKoombeaStyles = () => {
   .oa3_ui input.vf_buttons, .oa3_ui input[type="button"]:not(.ui3extended),
   .oa3_ui input[type="submit"]:not(.ui3extended) {
     background-color: var(--koombea-green);
+    border-radius: 4px;
   }
 
-  .item-active,
+  .oa3_ui .thirdLevelNavigation .tabItem.active.noLink, .oa3_ui .thirdLevelNavigation .tabItem.active a.tabLink, .oa3_ui .thirdLevelNavigation td.active>a.tabLink {
+    background: var(--koombea-green);
+    color: var(--white);
+  }
+
+  .oa3_ui .thirdLevelNavigation .tabItem.active a.tabLink:hover, .oa3_ui .thirdLevelNavigation a.tabLink:hover {
+    background: var(--koombea-dark-green);
+  }
+
+  .oa3_ui .thirdLevelNavigation a:link, .oa3_ui .thirdLevelNavigation a:visited, .oa3_ui .thirdLevelNavigation span.pagemenu span.menuAnchor {
+    color: var(--link);
+  }
+
+  .bp-toolbar-section-part .reportTypeActive {
+    border-bottom: 0 none !important;
+  }
+
+  .item-subtab.item-active,
   .item-link:hover {
-    border-color: var(--koombea-green) !important;
+    border-color: #dde0e3 !important;
+  }
+
+  .item-subtab {
+    border-left-color: var(--koombea-green) !important;
+  }
+
+  .nav-dialog {
+    background: var(--koombea-dark-green) !important;
+  }
+
+  .item-main.item-selected,
+  .item-main:hover,
+  .item-auxiliary:hover{
+    background: var(--koombea-green) !important;
+  }
+
+  .item-auxiliary {
+    background-color: var(--koombea-dark-green) !important;
+  }
+
+  .nav-dialog-section-header {
+    color: var(--white) !important;
   }
 
   .list-view-section-splitter {
     border-right-color: var(--koombea-green) !important;
+  }
+
+  .oa3_ui a:visited, .oa3_ui a:link, .oa3_ui a:active {
+    color: var(--link);
+  }
+
+  .oa3_ui a:hover {
+    color: var(--link-hover);
   }
 
   .svg.checkmarkSmall {
@@ -93,12 +151,12 @@ const addKoombeaStyles = () => {
 
   .oa3_ui div.notificationEnvelopeOutside div.notificationEnvelopeInside div.notificationTitle.notificationTitleConfirm {
     background-color: var(--koombea-green);
-    color: var(--koombea-dark-green);
+    color: var(--white);
   }
 
   .oa3_ui #page_footer.oa3_footer {
-    background-color: var(--koombea-green);
-    color: var(--koombea-dark-green);
+    background-color: var(--koombea-dark-green);
+    color: var(--white);
   }
 
   .oa3_ui .btn-oa:not(.ui3extended):hover,
@@ -112,7 +170,7 @@ const addKoombeaStyles = () => {
   .oa4:not([data-is-iframe]):not(.popupWindow):not(.in_pane):not(.integratedWindow):not(.inlineWindow) .bodyIndent .noBorder tr:nth-child(2),
   .oa4:not([data-is-iframe]):not(.popupWindow):not(.in_pane):not(.integratedWindow):not(.inlineWindow) .bodyIndent .noBorder tr:nth-child(6),
   .oa4:not([data-is-iframe]):not(.popupWindow):not(.in_pane):not(.integratedWindow):not(.inlineWindow) .bodyIndent .noBorder td[colspan="15"] {
-    background-color: var(--koombea-dark-green);
+    background-color: var(--koombea-green);
     color: var(--white);
   }
 
@@ -123,6 +181,10 @@ const addKoombeaStyles = () => {
 
   .column-filter-dropdown-active .inline-field-wrapper {
     background-color: var(--koombea-dark-green);
+  }
+
+  .column-filter-dropdown-active .inline-field-wrapper {
+    background-color: var(--koombea-dark-green) !important;
   }
 
   .svg.checkmarkGreenMedium {
@@ -146,37 +208,41 @@ const addKoombeaStyles = () => {
     filter: hue-rotate(45deg);
   }
 
-  .nav-list-wrapper[data-v-7fa901d4] {
-    background-color: #00343D !important;
+  .nav-list-wrapper {
+    background-color: var(--koombea-dark-green) !important;
   }
 
-   .company-logo-wrapper[data-v-26227760] {
-    background-color: #00343D !important;
+   .company-logo-wrapper {
+    background-color: var(--koombea-dark-green) !important;
   }
 
-  .company-logo-inner[data-v-26227760] {
+  .company-logo-inner {
     width: 60px !important;
     height: 60px !important;
     top: 8px !important;
     left: 8px !important;
   }
 
-  .company-logo[data-v-26227760] {
+  .company-logo {
     position: absolute;
     top: -2px;
     left: -2px;
   }
 
-  .item-auxiliary[data-v-591c513b] {
-    background-color: #00343D !important;
+  .TSCellHeader {
+    background: var(--koombea-green) !important;
   }
 
-  .TSCellHeader {
-    background: #26666B !important;
+  .TSGanttHeader2, .TSGanttHeader3, .TSGanttHeader5 {
+    background: var(--koombea-green);
+    border-left: 1px solid var(--koombea-dark-green);
+    border-top: 1px solid var(--koombea-dark-green);
   }
 
   .oa3_ui .viewFilterInline select.highlight, .oa3_ui .viewFilter select.highlight {
-    background-color: #48ABAA !important;
+    background-color: #F2F4F8 !important;
+    color: var(--koombea-dark-green);
+    border-color: #C7C7C7;
   }
 
   .resourceRowCell {
@@ -203,27 +269,52 @@ const addKoombeaStyles = () => {
   }
 
   #oa_view_booking_planner_envelope .GMGanttOvrOut, #oa_view_booking_planner_envelope div.GMGanttOvrIn {
-    background-color: #CC2E3B !important;
-    border-color: #CC2E3B !important;
+    background-color: var(--koombea-booking-overbook)!important;
+    border-color: var(--koombea-booking-overbook) !important;
   }
 
   #oa_view_booking_planner_envelope .resourceRowCell div:has(.GMGanttUnvIn),
   #oa_view_booking_planner_envelope .resourceRowCell div:has(.GMGanttHolIn) {
     border-right: 1px dotted rgb(178, 178, 178);
-    border-left: 1px dotted rgb(204, 204, 204);
+    border-left: 1px dotted rgb(178, 178, 178);
   }
 
   #oa_view_booking_planner_envelope .GMGanttHolOut, #oa_view_booking_planner_envelope div.GMGanttHolIn,
   #oa_view_booking_planner_envelope .GMGanttUnvOut, #oa_view_booking_planner_envelope div.GMGanttUnvIn,
   #oa_view_booking_planner_envelope .resourceRowCell div:has(.GMGanttUnvIn),
   #oa_view_booking_planner_envelope .resourceRowCell div:has(.GMGanttHolIn) {
-    background-color: rgb(242, 242, 242) !important;
+    background-color: var(--koombea-booking-holiday) !important;
   }
 
   #oa_view_booking_planner_envelope .resourceRowCell div:has(.GMGanttUnvIn),
   #oa_view_booking_planner_envelope .resourceRowCell div:has(.GMGanttHolIn) {
     border: 0 none !important;
     text-indent: -9999px !important;
+  }
+
+  #oa_view_booking_planner_envelope .GMGanttAvlOut, #oa_view_booking_planner_envelope div.GMGanttAvlIn {
+    background-color: var(--koombea-booking-available) !important;
+  }
+
+  #oa_view_booking_planner_envelope .GMGanttBillableConfirmed1Out, #oa_view_booking_planner_envelope div.GMGanttBillableConfirmed1In {
+    background-color: var(--koombea-booking-confirmed) !important;
+    border-color: var(--koombea-booking-confirmed);
+  }
+
+  #oa_view_booking_planner_envelope .GMGanttNonbillableConfirmed7Out, #oa_view_booking_planner_envelope div.GMGanttNonbillableConfirmed7In {
+    background-color: var(--koombea-booking-internal) !important;
+    border-color: var(--koombea-booking-internal);
+  }
+
+  #oa_view_booking_planner_envelope .GMGanttBillableTentative5Out, #oa_view_booking_planner_envelope div.GMGanttBillableTentative5In,
+  #oa_view_booking_planner_envelope .GMGanttNonbillableTentative5Out, #oa_view_booking_planner_envelope div.GMGanttNonbillableTentative5In{
+    background-color: var(--koombea-booking-tentative) !important;
+    border-color: var(--koombea-booking-tentative);
+  }
+
+  #oa_view_booking_planner_envelope .GMGanttUnavailable2Out, #oa_view_booking_planner_envelope div.GMGanttUnavailable2In {
+    background-color: var(--koombea-booking-pto) !important;
+    border-color: var(--koombea-booking-pto);
   }
 
   #oa_view_booking_planner_envelope .resourceRowCell .GMGanttBoxOut {
@@ -242,8 +333,22 @@ const addKoombeaStyles = () => {
     addStyle(css);
     return;
   }
-};
+}
 
 window.addEventListener('load', () => {
   addKoombeaStyles();
+  setInterval(function(){ // Converts booking % to h/d
+      var bookings = document.querySelectorAll('.GMGanttFixedIn, .GMGanttLeftIn, .GMGanttRightIn, .GMGanttBaseIn');
+      bookings.forEach(function(booking){
+          // Validates that the booking wasn't already converted and that it contains the % value
+          if(booking.innerHTML.split('h/d')[0] == booking.innerHTML && booking.innerHTML != '' && booking.innerHTML.match(/([0-9]*\.?[0-9]+)\s%/) != null){
+              var hours_per_day = Math.round((booking.innerHTML.match(/([0-9]*\.?[0-9]+)\s%/)[1]*8/100 + Number.EPSILON) * 100) / 100;
+              if(booking.innerHTML.match(/([0-9]*\.?[0-9]+)\s%/)[0] == booking.textContent) { // For allocation percentage bubbles at the top
+                  booking.innerHTML = hours_per_day + 'h/d';
+              } else { // For bookings
+                  booking.innerHTML = hours_per_day + 'h/d ' + booking.innerHTML;
+              }
+          }
+      });
+  },1000);
 });
